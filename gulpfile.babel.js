47b0d91d8002b747a0e809d8940683a3a2426dc3
Eskil Opdahl Nordland <eskil.nordland@gmail.com>
2017-11-23 17:50:46 +0100

    fixed build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,17 @@
 import _ from 'lodash';
+import del from 'del';
 import gulp from 'gulp';
 import nodemon from 'nodemon';
+import runSequence from 'run-sequence';
 import eslint from 'gulp-eslint';
 import babel from 'gulp-babel';
+import env from 'gulp-env';
+import util from 'gulp-util';
+
 
 const paths = {
-  scripts: [
-    'src/api/**/*.js',
-    'src/auth/**/*.js',
-    'src/models/**/*.js',
-    'src/config/**/*.js',
-    'src/*.js'
-  ],
+  src: 'src',
+  scripts: 'src/**/*.js',
   swagger: {
     config: 'src/config/default.yaml',
     spec: 'src/api/swagger/*.yaml'
@@ -20,13 +20,72 @@ const paths = {
   dist: 'dist'
 };
 
-gulp.task('dev', () => {
-    process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-    nodemon(`-w src src`);
+function onServerLog(log) {
+    console.log(util.colors.white('[') +
+        util.colors.yellow('nodemon') +
+        util.colors.white('] ') +
+        log.message);
+}
+
+
+/**
+ * Serve
+ */
+gulp.task('serve:dist', cb => {
+  runSequence(
+    'build',
+    'env:prod',
+    'start:prod',
+    cb);
 });
 
+gulp.task('serve', cb => {
+  runSequence(
+    'env:dev',
+    'start:dev',
+    cb);
+});
+
+/**
+* Start servers
+*/
+gulp.task('start:dev', () => {
+    nodemon(`-w ${paths.src} ${paths.src}`)
+      .on('log', onServerLog);
+});
+
+gulp.task('start:prod', () => {
+  nodemon(`-w ${paths.dist} ${paths.dist}`)
+    .on('log', onServerLog);
+});
+
+/**
+ * Env
+ */
+gulp.task('env:dev', () => {
+  env({
+    vars: { NODE_ENV: 'development' }
+  });
+});
+
+gulp.task('env:test', () => {
+  env({
+    vars: { NODE_ENV: 'test' }
+  });
+});
+
+gulp.task('env:prod', () => {
+  env({
+    vars: { NODE_ENV: 'production' }
+  });
+});
+
+/**
+ * Linting
+ */
+
 gulp.task('lint', () => {
-  return gulp.src(_.union(['src/**/*.js'], [`!${paths.test}`]))
+  return gulp.src(_.union([`${paths.scripts}`], [`!${paths.test}`]))
     .pipe(eslint({
       configFile: '.eslintrc.js'
     }))
@@ -34,7 +93,7 @@ gulp.task('lint', () => {
 });
 
 gulp.task('lint:fix', () => {
-  return gulp.src(_.union(['src/**/*.js'], [`!${paths.test}`]))
+  return gulp.src(_.union([`${paths.scripts}`], [`!${paths.test}`]))
     .pipe(eslint({
       configFile: '.eslintrc.js',
       fix: true
@@ -43,16 +102,35 @@ gulp.task('lint:fix', () => {
     .pipe(gulp.dest('./src'));
 });
 
+/**
+ * Build
+ */
+
+gulp.task('build', cb => {
+  runSequence(
+      'clean:dist',
+      'transpile',
+      'copy:yaml',
+      cb);
+});
+
 gulp.task('transpile', () => {
-  return gulp.src(_.union(['src/**/*.js'], [`!${paths.test}`]))
+  return gulp.src(_.union([`${paths.scripts}`], [`!${paths.test}`]))
     .pipe(babel({
-      presets: {
+      presets: [
         'es2015',
         'stage-0'
-      },
+      ],
       plugins: [
         'transform-runtime'
       ]
     }))
-    .pipe(gulp.dest('dist'));
-})
+    .pipe(gulp.dest(`${paths.dist}`));
+});
+
+gulp.task('copy:yaml', () => {
+  return gulp.src('src/**/*.yaml')
+    .pipe(gulp.dest(`${paths.dist}`));
+});
+
+gulp.task('clean:dist', () => del([`${paths.dist}/!(.git*|.openshift|Procfile)**`], {dot: true}));
